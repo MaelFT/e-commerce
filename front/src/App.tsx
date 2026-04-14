65e4878d8df1afb0904c3c4c1d7902914dbcc6d7
fmael <mael.fath@ynov.com>
2026-04-14 11:43:24 +0200

    feat(shopping):Setup shopping bag and paiments method with stripe

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,31 +1,38 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import { CartProvider } from './context/CartContext'
 import ProtectedRoute from './components/ProtectedRoute'
 import LoginPage    from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import HomePage     from './pages/HomePage'
 import AccountPage  from './pages/AccountPage'
 import ProductsPage from './pages/ProductsPage'
+import CartPage     from './pages/CartPage'
+import CheckoutSuccessPage from './pages/CheckoutSuccessPage'
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/"         element={<HomePage />} />
-          <Route path="/login"    element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route
-            path="/account"
-            element={
-              <ProtectedRoute>
-                <AccountPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <CartProvider>
+          <Routes>
+            <Route path="/"         element={<HomePage />} />
+            <Route path="/login"    element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/cart"     element={<CartPage />} />
+            <Route path="/checkout/success" element={<CheckoutSuccessPage />} />
+            <Route
+              path="/account"
+              element={
+                <ProtectedRoute>
+                  <AccountPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   )
