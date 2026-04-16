eceee0896d26b3610253aea4502d4b951b4505b1
flizze <alexandre.bel@ynov.com>
2026-04-16 12:11:03 +0200

    added footer

--- front/src/components/PublicLayout.tsx
+++ front/src/components/PublicLayout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react'
 import Header from './Header'
+import Footer from './Footer'
 
 interface PublicLayoutProps {
   children: ReactNode
@@ -12,6 +13,7 @@ export default function PublicLayout({ children }: PublicLayoutProps) {
       <main className="flex-grow flex flex-col">
         {children}
       </main>
+      <Footer />
     </div>
   )
 }
